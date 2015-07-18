911b67415ffbe9ae897004e5ddbe4be30573acd6
theme <xenotheme@gmail.com>
2015-07-18 15:16:33 +0800

    reduce header comments.

--- arduino/rcard/rcard.ino
+++ arduino/rcard/rcard.ino
@@ -1,9 +1,7 @@
-/*
-    Arduino PS1 Memory Card Reader - MemCARDuino, Shendo 2013
-
-    Thanks to:
+/*  Thanks to:
     Martin Korth of the NO$PSX - documented Memory Card protocol.
     Andrew J McCubbin - documented PS1 SPI interface.
+    Arduino PS1 Memory Card Reader - MemCARDuino, Shendo 2013
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -36,24 +34,6 @@
 
      If your card still isn't readable and it's a 3rd party card,
      you will need to supply it with 7.6 V extrenal power supply.
-
-     Interfacing a MemCARDuino:
-     ---------------------------
-     Communication is done at 38400 bps.
-     To check if the MemCARDuino is connected to the selected COM port send a GETID command.
-     Device should respond with IDENTIFIER.
-     Optionally you can send a GETVER to get the version of the firmware.
-
-     To Read a 128byte frame send a MCR command with MSB byte and LSB byte of the frame you want to read.
-     MemCARDduino will respond with 128 byte frame data, [MSB xor LSB xor Data] and Memory Card status byte.
-
-     To Write a frame send a MCW command with MSB byte and LSB byte, 128 byte data and [MSB xor LSB xor Data].
-     MemCARDduino will respond with Memory Card status byte.
-
-     Checking if the Memory Card is inserted:
-     -----------------------------------------
-     Read a frame of the card and verify the returned status byte.
-     If it's 0x47 then card is connected. If it's 0xFF card is not connected.
 */
 
 #include "Arduino.h"
