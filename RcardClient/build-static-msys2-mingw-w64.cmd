c38ad5dc55e80fd46dfb7bd844d70046e1cd8f33
theme <xenotheme@gmail.com>
2015-11-04 16:24:25 +0800

    static build: better build cmd script for msys2 qt5-static.

--- RcardClient/build-static-msys2-mingw-w64.cmd
+++ RcardClient/build-static-msys2-mingw-w64.cmd
@@ -1,4 +1,6 @@
-SET PATH=C:\msys64\usr\bin;C:\msys64\mingw64\bin;C:\msys64\mingw64\qt5-static\bin;%PATH%
+SET PATH=C:\msys64\mingw64\qt5-static\bin;C:\msys64\mingw64\bin;%PATH%
+SET LIBRARY_PATH=C:\msys64\mingw64\qt5-static\lib\
+
 SET QMAKESPEC=win32-g++
 SET BUILDDIR=..\build-RcardClient-static
 
@@ -7,6 +9,7 @@ cd %BUILDDIR%
 
 qmake "CONFIG+=release static win32" ..\RcardClient\RcardClient.pro
 
+mingw32-make clean
 mingw32-make
 
 pause
